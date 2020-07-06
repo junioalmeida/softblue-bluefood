fc35d558c82a8e8dcc3dc8d309d5d51b437d2b87
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2020-07-06 10:28:01 -0300

    Primeira entidade "Custumer".
    Entidade que representa um cliente na aplicacao.
    
    Utilizado uma classe mais generica "User" que representara um usuario do
    sistema, tanto para cliente quanto para restaurante.

---
+++ src/main/java/almeida/ferreira/junio/bluefood/domain/custumer/Custumer.java
@@ -0,0 +1,19 @@
+package almeida.ferreira.junio.bluefood.domain.custumer;
+
+import javax.persistence.Entity;
+
+import almeida.ferreira.junio.bluefood.domain.user.User;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@SuppressWarnings("serial")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@Entity
+public class Custumer extends User {
+
+	private String CPF;
+
+}
---
+++ src/main/java/almeida/ferreira/junio/bluefood/domain/user/User.java
@@ -0,0 +1,30 @@
+package almeida.ferreira.junio.bluefood.domain.user;
+
+import java.io.Serializable;
+
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@SuppressWarnings("serial")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User implements Serializable {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String phone;
+	
+	private String cep;
+	
+	private String password;
+}
