d201e0b776660f89d139da31bbababd1c0b8bc62
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2020-07-06 13:41:28 -0300

    Criada a conexão com o Banco de dados.

--- src/main/java/almeida/ferreira/junio/bluefood/domain/user/User.java
+++ src/main/java/almeida/ferreira/junio/bluefood/domain/user/User.java
@@ -3,6 +3,7 @@ package almeida.ferreira.junio.bluefood.domain.user;
 import java.io.Serializable;
 
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@MappedSuperclass
 public class User implements Serializable {
 	
 	@EqualsAndHashCode.Include
